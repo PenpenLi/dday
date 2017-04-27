283d0eb997ad7f14a7546dbec0335c4902705576
zhangyi <nickzy@163.com>
2017-04-27 19:12:42 +0800

    增加billboard 测试shader

---
+++ Assets/Script/Test/Quad.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quad 
+{
+	Mesh mesh;
+	Material material;
+	MeshFilter meshFilter;
+	MeshRenderer meshRenderer;
+	GameObject quadObj;
+
+	public void Init(Vector3 worldPos)
+	{
+		quadObj = new GameObject("Quad");
+
+		meshFilter = quadObj.AddComponent<MeshFilter>();
+		meshRenderer = quadObj.AddComponent<MeshRenderer>();
+
+		mesh = new Mesh();
+		mesh.name = "QuadMesh";
+
+		meshFilter.mesh = mesh;
+		meshRenderer.sharedMaterial = Resources.Load<Material>("Billboard/mat_billboard");
+
+
+
+		Vector3[] vertices = new Vector3[4]
+		{
+			worldPos,
+			worldPos,
+			worldPos,
+			worldPos
+		};
+
+		int[] indices = new int[6]
+		{
+			0, 2, 1, 0, 3, 2
+		};
+
+		Vector2[] uvs = new Vector2[4]
+		{
+			new Vector2(0, 0),
+			new Vector2(0, 1),
+			new Vector2(1, 1),
+			new Vector2(1, 0),
+		};
+
+		Vector2[] uv2s = new Vector2[4]
+		{
+			new Vector2(-0.5f, -0.5f),
+			new Vector2(0.5f, -0.5f),
+			new Vector2(0.5f, 0.5f),
+			new Vector2(-0.5f, 0.5f),
+		};
+
+		meshFilter.mesh.vertices = vertices;
+		meshFilter.mesh.triangles = indices;
+		meshFilter.mesh.uv = uvs;
+		meshFilter.mesh.uv2 = uv2s;
+
+
+	}
+
+	public void Update()
+	{
+//		if(meshRenderer)
+//		{
+//			meshRenderer.sharedMaterial.SetVector("right", Camera.main.transform.right);
+//			meshRenderer.sharedMaterial.SetVector("up", Camera.main.transform.up);
+//		}
+	}
+}
---
+++ Assets/Script/Test/TestQuad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestQuad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		Quad quad = new Quad();
+		quad.Init(Vector3.one);
+
+		quad = new Quad();
+		quad.Init(Vector3.zero);
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
